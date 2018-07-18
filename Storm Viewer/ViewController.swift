4277b086c7b045ab2038a1d9444ed1c0930988b0
Floyd <andreafloyd14@icloud.com>
2018-07-18 20:20:53 +0200

    commit

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix(prefix: "img")
+        }
     }
 
     override func didReceiveMemoryWarning() {
